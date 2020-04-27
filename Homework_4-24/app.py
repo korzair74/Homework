801d3a002de7fb0d99f2378678451ef35cf2f12a
Chris Nickel <chris.nickel.dev@gmail.com>
2020-04-27 11:57:10 -0400

    updated app

--- Homework_4-24/app.py
+++ Homework_4-24/app.py
@@ -26,9 +26,9 @@ ma = Marshmallow(app)
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False)
-    year = db.Column(db.Integer, unique=False)
-    rating = db.Column(db.String(160), unique=False)
-    starring = db.Column(db.String(180), unique=False)
+    year = db.Column(db.String(100), unique =False)
+    rating = db.Column(db.String(100), unique=False)
+    starring = db.Column(db.String(160), unique=False)
     genre = db.Column(db.String(100), unique=False)
     def __init__(self, title, year, rating, starring, genre):
         self.title = title
@@ -39,10 +39,10 @@ class Movie(db.Model):
         
 class MovieScheme(ma.Schema):
     class Meta:
-        fields = ('id', 'title', 'year', 'rating', 'starring', 'genre')
+        fields = ('id', 'title', 'year' 'rating', 'starring', 'genre')
 movie_schema = MovieScheme()
 movies_schema = MovieScheme(many=True)
-# Endpoint to create a new guide
+
 
 @app.route('/movie', methods=['POST'])
 def add_movie():
@@ -52,48 +52,45 @@ def add_movie():
   starring = request.json['starring']
   genre = request.json['genre']
   new_movie = Movie(title, year, rating, starring, genre)
-  
+
   db.session.add(new_movie)
   db.session.commit()
 
   movie = Movie.query.get(new_movie.id)
-
   return movie_schema.jsonify(movie)
 
-# Endpoint for all guides
-# @app.route('/movies', methods=['GET'])
-# def get_movies():
-#   all_movies = Movie.query.all()
-#   result = movie_schema.dump(all_movies)
-#   return jsonify(result)
+@app.route('/movies', methods=['GET'])
+def get_movies():
+  all_movies = Movie.query.all()
+  result = movies_schema.dump(all_movies)
+  return jsonify(result)
 
-# # Single Guide Endpoint
-# @app.route('/movie/<id>', methods=["GET"])
-# def get_movie(id):
-#   movie = Movie.query.get(id)
-#   return movie_schema.jsonify(movie)
+@app.route('/movie/<id>', methods=["GET"])
+def get_movie(id):
+  movie = Movie.query.get(id)
+  return movie_schema.jsonify(movie)
 
-# # Endpoint update guide
-# @app.route('/movie/<id>', methods=["PUT"])
-# def movie_update(id):
-#   movie = Movie.query.get(id)
-#   title = request.json['title']
-#   content = request.json['content']
 
-#   movie.title = title
-#   movie.content = content
+@app.route('/movie/<id>', methods=["PUT"])
+def movie_update(id):
+  movie = Movie.query.get(id)
+  title = request.json['title']
+  content = request.json['content']
 
-#   db.session.commit()
-#   return movie_schema.jsonify(movie)
+  movie.title = title
+  movie.content = content
 
-# @app.route('/movie/<id>', methods=["DELETE"])
-# def movie_delete(id):
-#   movie = Movie.query.get(id)
-#   db.session.delete(movie)
-#   db.session.commit()
-#   return "Movie was successfully Deleted"
+  db.session.commit()
+  return movie_schema.jsonify(movie)
 
-#   return
+@app.route('/movie/<id>', methods=["DELETE"])
+def movie_delete(id):
+  movie = Movie.query.get(id)
+  db.session.delete(movie)
+  db.session.commit()
+  return "Movie was successfully Deleted"
+
+  return
 
 if __name__ == '__main__':
     app.run(debug=True)
